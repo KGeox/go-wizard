0e1a9a153f55960dfcb6a15a1a89a06ccb482753
Georges <georgeskuetche93@gmail.com>
2026-06-04 15:39:32 +0100

    an adventure game
    Adding fonts, coin and its behaviour, adding global variables and script, adding layers

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -4,7 +4,6 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-@onready var DetectionArea := $DetectionArea
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -12,10 +11,3 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 		
 	move_and_slide()
-
-
-func _on_time_to_detect_timeout() -> void:
-	var player := get_tree().get_first_node_in_group("player")
-	var detected :Array[Node3D] = DetectionArea.get_overlapping_bodies()
-	if player in detected:
-		velocity.x = move_toward(velocity.x, 10, SPEED)
