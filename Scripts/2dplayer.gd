cf8a8c00b8c2ecedfff7674d67a6d354d2631c44
Georges <georgeskuetche93@gmail.com>
2026-06-17 18:24:55 +0100

    2d to 3d adventure game
    designing m 2d level and organising my files

--- Scripts/2dplayer.gd
+++ Scripts/2dplayer.gd
@@ -1,14 +1,19 @@
 extends CharacterBody2D
+class_name Player2d
 
-
-const SPEED = 300.0
+const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
+var current_interactable_obj :Node = null
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+	if current_interactable_obj != null:
+		if Input.is_action_just_pressed("interact"):
+			current_interactable_obj.interact()
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -16,7 +21,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
