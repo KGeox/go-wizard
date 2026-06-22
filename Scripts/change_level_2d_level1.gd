e8a63ed4e1a83893371b88572d6c87bae452f13d
Georges <georgeskuetche93@gmail.com>
2026-06-22 07:57:33 +0100

    Adding 2D items and making a new level1
    Adding 2D scene level 1 and adding 2d coins, killzone 2d and change level2d

---
+++ Scripts/change_level_2d_level1.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player2d:
+		body.current_interactable_obj = self
+		print("you Entered")
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body is Player2d:
+		if body.current_interactable_obj == self:
+			body.current_interactable_obj =null
+			print("You went out")
+			
+func interact() -> void:
+	get_tree().change_scene_to_file("res://Scenes/2D_Levels/2DLevel.tscn")
+	Global.save_game()
