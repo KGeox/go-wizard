6ff69baf79870009345ed81fa24c686ddfe25452
Georges <georgeskuetche93@gmail.com>
2026-07-02 13:09:18 +0100

    level 1 updates towards aplayeble game
    temporal removal on level 1 boss, trying to make the boss move( not yet successful)

--- Scripts/boss_2d_1.gd
+++ Scripts/boss_2d_1.gd
@@ -3,16 +3,17 @@ extends CharacterBody2D
 
 const SPEED = 50.0
 const JUMP_VELOCITY = -400.0
-var player : Player2d1
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	if player:
-		print("ok")
-		var direction := (player.global_position - global_position).normalized()
-		velocity = direction * SPEED
+
 		
 	move_and_slide()
+
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	if body is Player2d1:
+		print(body.global_position)
